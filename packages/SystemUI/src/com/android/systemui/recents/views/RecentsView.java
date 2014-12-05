d3bf74bf16aef73274d8188d7451c8ff65b668c5
AragaoAnderson <andersonaragao@me.com>
2014-12-05 18:58:52 -0200

    Revert "Framework: Use clearRecents() to close tasks"
    This reverts commit 02d7e10cfa2ebaa8ec631779fa50c3fca4921b6c.

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -152,7 +152,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             View child = getChildAt(i);
             if (child != mSearchBar) {
                 TaskStackView stackView = (TaskStackView) child;
-                stackView.clearRecents();
+                stackView.dismissAllTasks();
             }
         }
     }
@@ -163,7 +163,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             View child = getChildAt(i);
             if (child != mSearchBar) {
                 TaskStackView stackView = (TaskStackView) child;
-                stackView.clearRecents();
+                stackView.dismissAllTasks();
             }
         }
     }
