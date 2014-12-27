a676375948cdf641bbd7e7136c76d127c1238da7
AragaoAnderson <andersonaragao@me.com>
2014-12-27 20:11:44 -0200

    Revert "Framework: Add enabler for Navbar Recents"
    This reverts commit db9d5a9e2cb5b5fd1546130d48aa8849f98e5d22.
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1237,7 +1237,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private View.OnClickListener mRecentsClickListener = new View.OnClickListener() {
         public void onClick(View v) {
             awakenDreams();
-            if(isRecentAppsVisible() && hasRecentApps() && clearAllRecentsNavbarEnabled()) {
+            if(isRecentAppsVisible() && hasRecentApps()) {
                 clearRecentApps();
             } else {
                 toggleRecentApps();
@@ -3542,13 +3542,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mHeadsUpObserver.onChange(true);
     }
-
-    /** Check if Clear All Recents is enabled */
-    public boolean clearAllRecentsNavbarEnabled() {
-        return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.CLEAR_ALL_RECENTS_NAVBAR_ENABLED, 1) != 0;
-    }
-
 
     private void setControllerUsers() {
         if (mZenModeController != null) {
